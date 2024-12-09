0052a67207623fc5f01778a505edb3d685888748
LearnerAdityaa <adityatharudev@gmail.com>
2024-12-09 20:39:33 +0545

    feat:added 404 error handling and global error handling middleware

--- index.js
+++ index.js
@@ -11,7 +11,17 @@ app.use(cors());
 
 app.use("/api/v1", router);
 
-
+// Handle 404 errors
+app.all("*", (req, res, next) => {
+  next(new expressError(404, "Page Not Found"));
+});
+// Error handler middleware
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Internal Server Error";
+  // console.error(`Error: ${message}, Status Code: ${status}`, err);
+  res.status(status).json({ message });
+});
 const PORT = process.env.PORT | 5454;
 //establish connection
 connection();
