abd13111f0bed138757b09260fd7e3142e23d446
LearnerAdityaa <adityatharudev@gmail.com>
2024-12-09 20:15:10 +0545

    feat:added folder structure

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT | 5454;
-//establish connection 
+//establish connection
 connection();
 // server start
 app.listen(PORT, () => {
