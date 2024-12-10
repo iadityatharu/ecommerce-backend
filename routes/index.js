02ef71e16211a9f976eb27925211e046fe3d2289
LearnerAdityaa <adityatharudev@gmail.com>
2024-12-10 21:24:55 +0545

    fix:fix bugs

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 import express from "express";
 const router = express.Router();
+import userSignup from "../controllers/userSignup.js";
+import wrapAsync from "../utils/wrapAsync.js";
+router.post("/signup", wrapAsync(userSignup));
 
 export default router;
