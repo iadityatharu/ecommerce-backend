3de33c53b0c1e22d90acf1a0e5ea9be37a1a3d83
LearnerAdityaa <adityatharudev@gmail.com>
2024-12-12 14:08:36 +0545

    feat:added route for login

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 const router = express.Router();
 import { userSignup } from "../controllers/userSignup.js";
+import { userSignin } from "../controllers/userSignin.js";
 import { wrapAsync } from "../utils/wrapAsync.js";
 router.post("/signup", wrapAsync(userSignup));
-
+router.post("/signin", wrapAsync(userSignin));
 export default router;
