67d36fef2442ab36d5bc21a1d428dac7044dc5aa
LearnerAdityaa <adityatharudev@gmail.com>
2024-12-13 10:02:18 +0545

    feat:fixed cors

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ import expressError from "./utils/expressError.js";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
