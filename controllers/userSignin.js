f501b07c6e9d9d79c686ba444f1453d1abc3ac4f
LearnerAdityaa <adityatharudev@gmail.com>
2024-12-13 09:22:28 +0545

    feat:added login function using http-cookie only

--- controllers/userSignin.js
+++ controllers/userSignin.js
@@ -16,5 +16,20 @@ export const userSignin = async (req, res) => {
   if (checkPassword !== true) {
     throw new expressError(401, true, "Invalid credentials");
   }
-  return res.status(200).json({ status: true, message: "Login successful" });
+  const tokenData = {
+    _id: user._id,
+    role: user.role,
+  };
+
+  const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET, {
+    expiresIn: 5 * 60 * 1000,
+  });
+  const tokenOption = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .cookie("token", token, tokenOption)
+    .status(200)
+    .json({ status: true, message: "Login successful", data: token });
 };
