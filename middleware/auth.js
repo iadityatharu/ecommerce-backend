8e9a7edfb0287b16926b315517832b21b1f85b76
LearnerAdityaa <adityatharudev@gmail.com>
2024-12-14 13:10:54 +0545

    feat:added auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,18 @@
-export const auth = async (req, res) => {
+import jwt from "jsonwebtoken";
+import expressError from "../utils/expressError.js";
+export const auth = async (req, res, next) => {
   try {
-    const token = req.cookies?.token;
-    console.log(token);
+    const token = req.cookies.token;
+    if (!token) {
+      throw new expressError(401, "Authentication token is expired");
+    }
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+      if (err) {
+        throw new expressError(403, "Token is expired, Please sign in again");
+      }
+      req.user = user;
+      next();
+    });
   } catch (error) {
     res.status(400).json({
       message: "Invalid credentials",
