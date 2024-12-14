fbae037da8a2f932d0550a5cb380239a2a939db0
LearnerAdityaa <adityatharudev@gmail.com>
2024-12-14 16:41:10 +0545

    feat:added feature to get user detail

--- controllers/userDetail.js
+++ controllers/userDetail.js
@@ -1,5 +1,9 @@
 import User from "../models/user.js";
 import expressError from "../utils/expressError.js";
 export const userDetail = async (req, res) => {
-    const userId=req.user;
+  const userId = req.user;
+  if (!userId) {
+    throw new expressError(404, true, "User not found");
+  }
+  const user = await User.findById({ userId });
 };
