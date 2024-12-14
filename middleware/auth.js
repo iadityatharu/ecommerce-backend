bd71ef1c27565328d04aec9eb948bd40a2238f36
LearnerAdityaa <adityatharudev@gmail.com>
2024-12-14 13:14:18 +0545

    feat:added error flag while error in auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,11 +4,15 @@ export const auth = async (req, res, next) => {
   try {
     const token = req.cookies.token;
     if (!token) {
-      throw new expressError(401, "Authentication token is expired");
+      throw new expressError(401, true, "Authentication token is expired");
     }
     jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
       if (err) {
-        throw new expressError(403, "Token is expired, Please sign in again");
+        throw new expressError(
+          403,
+          true,
+          "Token is expired, Please sign in again"
+        );
       }
       req.user = user;
       next();
