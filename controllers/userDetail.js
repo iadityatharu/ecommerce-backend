0a83aa7d2f8b4a0ab5cd1ae0f36b4d2be7ffbd6c
Aditya Tharu <159002878+iadityatharu@users.noreply.github.com>
2024-12-15 11:20:11 +0545

    feat:added function to fetch user data

--- controllers/userDetail.js
+++ controllers/userDetail.js
@@ -1,13 +1,18 @@
 import User from "../models/user.js";
 import expressError from "../utils/expressError.js";
 export const userDetail = async (req, res) => {
-  const userId = req.user;
-  if (!userId) {
+  const { _id } = req.user;
+  console.log(_id);
+  if (!_id) {
     throw new expressError(404, true, "User not found");
   }
-  const user = await User.findById({ userId });
-  if(!user)
-  {
+  const user = await User.findById({ _id });
+  if (!user) {
     throw new expressError(404, true, "User not found");
   }
+  return res.status(200).json({
+    status: true,
+    message: "User detail fetch successfully",
+    data: user,
+  });
 };
