76bdf3a7e76399569ac0a2e3720b44125e8aecc9
Aditya Tharu <159002878+iadityatharu@users.noreply.github.com>
2024-12-15 11:32:21 +0545

    fix:remove useless log

--- controllers/userDetail.js
+++ controllers/userDetail.js
@@ -2,7 +2,6 @@ import User from "../models/user.js";
 import expressError from "../utils/expressError.js";
 export const userDetail = async (req, res) => {
   const { _id } = req.user;
-  console.log(_id);
   if (!_id) {
     throw new expressError(404, true, "User not found");
   }
