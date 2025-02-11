c9425e9d500816cfa4a795a29e96d5f44a767cc1
Aditya Tharu <159002878+iadityatharu@users.noreply.github.com>
2025-02-11 21:44:32 +0545

    fix:fix bugs

--- controllers/userSignup.js
+++ controllers/userSignup.js
@@ -5,9 +5,6 @@ import bcrypt from "bcryptjs";
 export const userSignup = async (req, res) => {
   const { email, password, name, role } = req.body;
   const existingUser = await User.findOne({ email });
-  if (existingUser) {
-    throw new expressError(409, true, "User already exists");
-  }
  
   const hashPassword = await bcrypt.hash(password, 10);
   if (!hashPassword) {
