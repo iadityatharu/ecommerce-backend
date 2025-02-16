6e7c014290b65088b5fb38dc0e7b3c710788e162
Aditya Tharu <159002878+iadityatharu@users.noreply.github.com>
2025-02-16 21:03:43 +0545

    fix:fix bugs

--- controllers/userSignup.js
+++ controllers/userSignup.js
@@ -8,7 +8,9 @@ export const userSignup = async (req, res) => {
   if (existingUser) {
     throw new expressError(409, true, "User already exists");
   }
-
+  if (!email || !name || !password || !role) {
+    throw new expressError(400, true, "All fields are required");
+  }
   const hashPassword = await bcrypt.hash(password, 10);
   if (!hashPassword) {
     throw new expressError(400, true, "Hash password required");
