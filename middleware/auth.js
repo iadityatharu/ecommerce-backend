074f6e7cdb6bd6f9f5dd01b72021c7f3b9aa7475
Aditya Tharu <159002878+iadityatharu@users.noreply.github.com>
2025-07-17 20:42:47 +0545

    Update auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import expressError from "../utils/expressError.js";
+export const auth = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    console.log(token);
+    if (!token) {
+      throw new expressError(401, true, "Authentication token is expired");
+    }
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+      if (err) {
+        throw new expressError(
+          403,
+          true,
+          "Token is expired, Please sign in again"
+        );
+      }
+      req.user = user;
+      next();
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Invalid credentials",
+      data: [],
+      error: true,
+      success: false,
+    });
+  }
+};
