65089febe7d12f44e9551cd5198ad3b67de12ab5
Aditya Tharu <159002878+iadityatharu@users.noreply.github.com>
2025-07-31 20:12:35 +0545

    Update userDetail.js

--- controllers/userDetail.js
+++ controllers/userDetail.js
@@ -1,1 +1,20 @@
 
+import User from "../models/user.js";
+import expressError from "../utils/expressError.js";
+export const userDetail = async (req, res) => {
+  const { _id } = req.user;
+  if (!_id) {
+    throw new expressError(404, true, "User not found");
+  }
+  const user = await User.findById({ _id }).select(
+    "-_id -password -createdAt -updatedAt"
+  );
+  if (!user) {
+    throw new expressError(404, true, "User not found");
+  }
+  return res.status(200).json({
+    status: true,
+    message: "User detail fetch successfully",
+    data: user,
+  });
+};
